81c18c68519544b290334d4dcaa9945c6ead19dd
TR1ZZY <150307948+TR1ZY@users.noreply.github.com>
2025-10-04 12:16:22 -0400

    Updated the dictionary of users
    Updated the dictionary to users and allowed passwords. also added passwords

--- discount.py
+++ discount.py
@@ -1,17 +1,30 @@
 import random
+# import getpass
 
 def generate_discount_code():
     return str(random.randint(100000000, 999999999))
 
-allowed_domains = ["@school.edu", "@stu.edu", "@.edu", "@college.edu", "@uni.edu"]
+users = {
+    "alice@school.edu": "12x129",
+    "bob@college.edu": "1x1229",
+    "carol@uni.edu": "122x29"
+}
 
 while True:
-    email: str = input("Please enter your email: ")
-    if any (email.endswith(domain) for domain in allowed_domains):
-        discount = True
-        print("Your student discount code is", generate_discount_code())
+    email: str = input("Enter your email: ")
+
+    if email in users:
+        # print("Email Accepted")
         break
     else:
-        discount = False
-        print("Please enter a valid student email address")
+        print("Invalid Email")
 
+while True:
+    password = input('Enter your password: ')
+
+    if password == users[email]:
+        # print("Login Successful")
+        print("Your discount code is", generate_discount_code())
+        break
+    else:
+        print("Invalid Password")
